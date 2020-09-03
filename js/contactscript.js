127f7865913d7084c438aa6ca353482fc2a44864
Carlos <carlosbenitez86753@yahoo.com>
2020-09-03 01:11:47 -0400

    fixed the footer and changed the css

--- js/contactscript.js
+++ js/contactscript.js
@@ -1,24 +1,8 @@
-//I want to store and send myself an email with the form information
-
-//Pull information from form
 let button = document.querySelector(".btn")
-let userInput = document.querySelector("#user-input")
-
-var getInfo = $('#user-input').serializeArray();
-    console.log(getInfo)
 
 button.addEventListener('click', (e) => {
-    let submitInfo = userInput.value;
-        console.log(submitInfo);
 
-//stores submit value in session storage
-sessionStorage.setItem("submit", submit);
-    window.location.href='thankyou.html';
+    window.location.href = "mailto:carlosbenitez86753@yahoo.com?subject=" + document.getElementById('Subject').value + "&body=" +
+    document.getElementById('Message').value
+    window.location.href = 'thankyou.html';
 });
-
-let submit = sessionStorage.getItem("submit");
-    console.log(submit);
-
-
-//Send information to my email
-
